d2f51f1e14021bc31c8d204a61ffe8c639a999e0
SaraGonSilvaa <sara.silva@subvisual.academy>
2026-04-07 21:40:05 +0100

    Feature/get clubs endpoint (#1)
    Adds the GET/Clubs route to return the list of clubs.

--- api/app.js
+++ api/app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Registar a rota:
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var clubsRouter = require('./routes/clubs');
 
 var app = express();
 
@@ -19,8 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Quando alguém aceder ao /clubs, usa o clubsRouter
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/clubs', clubsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ api/routes/clubs.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let router = express.Router();
+const fs = require('fs');
+
+const data = fs.readFileSync('./clubs.json', 'utf8');
+const clubs = JSON.parse(data);
+
+
+router.get('/', function(req,res) {
+res.json(clubs);
+})
+module.exports = router;
