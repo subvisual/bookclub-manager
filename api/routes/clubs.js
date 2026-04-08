7a1bbee714007691a7a7a6e366d13456f0f898cf
Sara Silva <sara.silva@subvisual.academy>
2026-04-08 22:41:03 +0100

    Adds the POST/Clubs route to insert a new club

--- api/routes/clubs.js
+++ api/routes/clubs.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 const fs = require('fs');
+const { stringify } = require('querystring');
 
 const data = fs.readFileSync('./clubs.json', 'utf8');
 const clubs = JSON.parse(data);
@@ -9,4 +10,14 @@ const clubs = JSON.parse(data);
 router.get('/', function(req,res) {
 res.json(clubs);
 })
+
+
+router.post('/', function (req, res){
+    const newBookClub = req.body;
+    clubs.clubs.push(newBookClub);
+    fs.writeFileSync('./clubs.json', JSON.stringify(clubs));
+    res.json(newBookClub);
+    
+});
+
 module.exports = router;
