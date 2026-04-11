412be20d56a77d7b8f4c2e50f2a3303ecfec9c3e
Sara Silva <sara.silva@subvisual.academy>
2026-04-11 23:32:06 +0100

    Add DELETE/clubs route to delete a club

--- api/routes/clubs.js
+++ api/routes/clubs.js
@@ -3,6 +3,7 @@ let router = express.Router();
 const fs = require('fs');
 const { stringify } = require('querystring');
 
+
 const data = fs.readFileSync('./clubs.json', 'utf8');
 const clubs = JSON.parse(data);
 
@@ -19,5 +20,17 @@ router.post('/', function (req, res){
     res.json(newBookClub);
     
 });
+
+router.delete('/:id', function(req, res){
+    // Para ir buscar o id do pedido, é devolvido em req.params.id e armazenado em id 
+    const id = Number(req.params.id);
+    // Filtro os clubs e armazeno se for diferente do id.
+    const update = clubs.clubs.filter(club => club.id !==id);
+    
+    clubs.clubs = update
+    fs.writeFileSync('./clubs.json', JSON.stringify(clubs));
+   
+    res.json(update);
+})
 
 module.exports = router;
