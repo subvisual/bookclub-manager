ab134324c9e254c758a31b012d7b6d86b649fd4c
SaraGonSilvaa <sara.silva@subvisual.academy>
2026-04-19 20:12:31 +0100

    set-up-biome (#6)

--- Frontend/src/callApi.ts
+++ Frontend/src/callApi.ts
@@ -1,35 +1,28 @@
 export interface BookClub {
-    id: number;
-    name: string; 
-    description: string;
-    // Vou buscar na interface abaixo
-    currentBook: Book;
-    // Lista de livros no []
-    upcomingBooks: Book[];
-    pastBooks: Book[];
+	id: number;
+	name: string;
+	description: string;
+	// Vou buscar na interface abaixo
+	currentBook: Book;
+	// Lista de livros no []
+	upcomingBooks: Book[];
+	pastBooks: Book[];
 }
 
 export interface Book {
-title: string;
-author: string;
+	title: string;
+	author: string;
 }
 
+export const url = "http://localhost:3000/clubs";
 
-
-
-export const url = 'http://localhost:3000/clubs';
-
-// : Tipar a função. Esta devolve uma promise que por sua vez devolve o [] de clubs. 
+// : Tipar a função. Esta devolve uma promise que por sua vez devolve o [] de clubs.
 // É chamada no react
 export function getBookClubs(): Promise<BookClub[]> {
-
-// GET por defeito
-return fetch(url)
-.then((response) => {
-    if (response.status == 200) {
-       
-return response.json()
-}
-    }
-    )
+	// GET por defeito
+	return fetch(url).then((response) => {
+		if (response.status == 200) {
+			return response.json();
+		}
+	});
 }
