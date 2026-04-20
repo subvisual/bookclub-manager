403e2e74589e5d7bcce389e6aaf14bb3e043ecb0
Sara Silva <sara.silva@subvisual.academy>
2026-04-20 22:31:35 +0100

    add useClubs hook

--- Frontend/src/useClubs.ts
+++ Frontend/src/useClubs.ts
@@ -1,5 +1,20 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import type { BookClub } from "./callApi";
 import { getBookClubs } from "./callApi";
 
+export function useClubs() {
+	// Guardo os clubs no estado
+	const [clubs, setClubs] = useState<BookClub[]>([]);
 
-const [clubs, setClubs] = useState<BookClub[]>([])
+	useEffect(
+		() => {
+			// A função do call api, vai buscar os clubs
+			getBookClubs()
+				// passa os clubs para setClubs
+				.then(setClubs);
+		},
+		// Para só executar uma vez
+		[],
+	);
+	return clubs;
+}
