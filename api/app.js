9b5c24d93a827e15c498f578571ac20f56a9987b
SaraGonSilvaa <sara.silva@subvisual.academy>
2026-04-24 22:27:33 +0100

    Feature/club list component (#10)

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+//Importar cors instalado
+var cors = require("cors");
 
 //Registar a rota:
 var indexRouter = require("./routes/index");
@@ -20,6 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Para usar cors em todos os pedidos
+app.use(cors());
 
 //Quando alguém aceder ao /clubs, usa o clubsRouter
 app.use("/", indexRouter);
