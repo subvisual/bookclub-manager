660551041bf3f15c6f1d14b7e4bc5ef5854bccfb
SaraGonSilvaa <sara.silva@subvisual.academy>
2026-04-30 18:42:19 +0100

    Feature/delete club (#11)

--- Frontend/src/ClubCard.tsx
+++ Frontend/src/ClubCard.tsx
@@ -1,8 +1,19 @@
 //Importo a interface
 import type { BookClub } from "./callApi";
 
+//Para receber a function deleteClub. Extende BookClub
+type ClubCardProps = BookClub & {
+	deleteClub: (id: number) => void;
+};
+
 // Component para mostrar Club - {props} do BookClub
-export function ClubCard({ name, description, currentBook }: BookClub) {
+// Props deleteClub adicionada
+export function ClubCard({
+	name,
+	description,
+	currentBook,
+	deleteClub,
+}: ClubCardProps) {
 	return (
 		<div>
 			<h1>{name}</h1>
--- Frontend/src/ClubList.tsx
+++ Frontend/src/ClubList.tsx
@@ -3,10 +3,12 @@ import { useClubs } from "./useClubs";
 
 export function ClubList() {
 	// uso o hook criado em useClubs
-	const clubs = useClubs();
+	// Atualizei o ClubList com o delete
+	const { clubs, deleteClub } = useClubs();
 	// percorro o array através de map
 	return clubs.map((club) => {
 		// Passo para o card (key por causa do react)
-		return <ClubCard key={club.id} {...club} />;
+		//Atualizei o card com
+		return <ClubCard key={club.id} {...club} deleteClub={deleteClub} />;
 	});
 }
