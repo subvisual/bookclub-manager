8671dc02d32d34aac4d25b525d6d1bc33770ff9c
SaraGonSilvaa <sara.silva@subvisual.academy>
2026-05-13 20:46:23 +0100

    Feature/clubs display base (#17)

--- Frontend/src/ClubCard.tsx
+++ Frontend/src/ClubCard.tsx
@@ -4,7 +4,7 @@ import type { BookClub } from "./callApi";
 //Para receber a function deleteClub. Extende BookClub
 type ClubCardProps = BookClub & {
 	deleteClub: (id: number) => void;
-	updateClub: (id: number, updateClub: Partial<BookClub>) => void;
+	updateClub: (id: number, updatedClub: Partial<BookClub>) => void;
 };
 
 // Component para mostrar Club - {props} do BookClub
@@ -17,8 +17,8 @@ export function ClubCard({
 	updateClub,
 }: ClubCardProps) {
 	return (
-		<div>
-			<h1>{name}</h1>
+		<div className="club-card">
+			<h2>{name}</h2>
 			<p>{description}</p>
 			<p>{currentBook.title}</p>
 			<p>{currentBook.author}</p>
--- Frontend/src/ClubList.tsx
+++ Frontend/src/ClubList.tsx
@@ -4,18 +4,25 @@ import { useClubs } from "./useClubs";
 export function ClubList() {
 	// uso o hook criado em useClubs
 	// Atualizei o ClubList com o delete
-	const { clubs, deleteClub, updateClub /*,createClub,*/ } = useClubs();
+	const { clubs, deleteClub, updateClub } = useClubs();
 	// percorro o array através de map
-	return clubs.map((club) => {
-		// Passo para o card (key por causa do react)
-		//Atualizei o card com
-		return (
-			<ClubCard
-				key={club.id}
-				{...club}
-				deleteClub={deleteClub}
-				updateClub={updateClub} /*createClub={createClub}*/
-			/>
-		);
-	});
+	return (
+		<>
+			<h1>Book Clubs</h1>
+			<div className="club-list">
+				{clubs.map((club) => {
+					// Passo para o card (key por causa do react)
+					//Atualizei o card com
+					return (
+						<ClubCard
+							key={club.id}
+							{...club}
+							deleteClub={deleteClub}
+							updateClub={updateClub} /*createClub={createClub}*/
+						/>
+					);
+				})}
+			</div>
+		</>
+	);
 }
