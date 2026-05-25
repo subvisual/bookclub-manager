021a2199a1888c91d0314ee7ae777ae72f1ed996
SaraGonSilvaa <sara.silva@subvisual.academy>
2026-05-25 20:17:34 +0100

    Feature/move state to app (#23)

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,12 +5,19 @@ import { useClubs } from "./useClubs";
 
 function App() {
 	// Uso o hook criado em useClubs para ir buscar a lista de clubs e selectedClub
-	const { clubs, selectedClub } = useClubs();
+	// Fui buscar tb deleteClub e updateClub, uma vez que subiu do ClubList para o App
+	const { clubs, selectedClub, showClub, deleteClub, updateClub } = useClubs();
 	// Localizo o club por id, que é igual ao do selectedCLub
 	const club = clubs.find((club) => club.id === selectedClub);
 	// Se o selectedClub for igual a null ou se o club não existir(o id n existe na lista), mostra CLubList, se não ClubDetails
 	return selectedClub == null || !club ? (
-		<ClubList />
+		// Preciso passar showClub para ClubList para o fluxo App -> ClubList -> ClubCard -> DetailsButton
+		<ClubList
+			showClub={showClub}
+			deleteClub={deleteClub}
+			updateClub={updateClub}
+			clubs={clubs}
+		/>
 	) : (
 		<ClubDetails {...club} />
 	);
